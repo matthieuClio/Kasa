e867c2454a1438a080a6d8141228fd2b905b0555
Matthieu <matthieu.clio@gmail.com>
2023-11-24 13:41:39 +0100

    Feat (accomodation) : finish gallery component.

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -5,31 +5,37 @@ export default function Gallery ({ picturesSrc }) {
     let [currentPicture, setCurrentPicture] = useState(0)
     const nbPictures = picturesSrc.length
 
-    function changePicture () {
-        setCurrentPicture(currentPicture +1)
+    // Change picture
+    // (not in a script file cause of State modification)
+    function changePicture (pictureToDisplay) {
+        // We are to the first and want go to the last picture
+        if (pictureToDisplay < 0) {
+            setCurrentPicture(nbPictures -1)
 
-        if (currentPicture < 0) {
-            setCurrentPicture(currentPicture = nbPictures -1)
-        }
-        if (currentPicture >= nbPictures -1) {
-            setCurrentPicture(currentPicture = 0)
+         // We are to the last and want go to the first picture
+        } else if (pictureToDisplay > nbPictures -1) {
+            setCurrentPicture(0)
+        
+        // We add or we subtract
+        } else {
+            setCurrentPicture(pictureToDisplay)
         }
     }
 
     return (
         <figure className="gallery">
             {/* Arrow left icon */}
-            <img src="/image/leftArrow.svg" onClick={changePicture} className="gallery__arrow-left" alt={'flèche de gauche'} />
+            <img src="/image/leftArrow.svg" onClick={() => changePicture(currentPicture -1)} className="gallery__arrow-left" alt={'flèche de gauche'} />
             
             {/* Picture */}
             <img src={picturesSrc[currentPicture]} className="gallery__image" alt="location" />
             
             {/* Arrow right icon */}
-            <img src="/image/rightArrow.svg" className="gallery__arrow-right" alt={'flèche de droite'} />
+            <img src="/image/rightArrow.svg" onClick={() => changePicture(currentPicture +1)} className="gallery__arrow-right" alt={'flèche de droite'} />
             
             {/* Image number */}
             <div className="gallery__image-number">
-                1/4
+                {`${currentPicture +1}/${nbPictures}`}
             </div>
         </figure>
     )
