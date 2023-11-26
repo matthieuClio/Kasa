3102231894e9b042a06d474fd97c7d8afac5df31
Matthieu <matthieu.clio@gmail.com>
2023-11-26 14:13:20 +0100

    Feat (about) : use component Background in About page, modify component Background.

--- src/component/Background.jsx
+++ src/component/Background.jsx
@@ -4,7 +4,7 @@ export default function Background ({ title, imagePath }) {
     return (
         <figure className="image-container">
           <h1 className="image-container__title">{title}</h1>
-          <img src={imagePath} />
+          <img src={imagePath} className="image-container__image" />
           <div className="image-container__background"></div>
         </figure>
     )
--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,8 +1,12 @@
 // Component
+import Background from '../component/Background'
+
 export default function About () {
+    const urlPicture = '/image/about_background.jpg'
     return (
         <>
             <main className="about">
+                <Background imagePath={urlPicture}/>
                 <div>A propos page</div>
             </main>
         </>
